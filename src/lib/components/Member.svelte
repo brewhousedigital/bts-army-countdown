389b2f6a536fd76faffe31c8595b9e634183ccd2
Brewhouse Digital <zach@brewhouse.digital>
2023-04-14 19:43:40 -0500

    Initial deployment

---
+++ src/lib/components/Member.svelte
@@ -0,0 +1,57 @@
+<script>
+  import dayjs from "dayjs";
+
+  export let name = "";
+  export let start = "";
+  export let end = "";
+
+  const currentDate = dayjs();
+  const startDate = dayjs(start);
+  const endDate = dayjs(end);
+
+  let totalDaysRequired = endDate.diff(startDate, "day")
+  if(isNaN(totalDaysRequired)) totalDaysRequired = 0
+
+  let daysCurrentlyServed = currentDate.diff(startDate, "day")
+  if(isNaN(daysCurrentlyServed)) daysCurrentlyServed = 0
+
+  let percentDifference = Math.ceil((daysCurrentlyServed / totalDaysRequired) * 100)
+  if(isNaN(percentDifference)) percentDifference = 0
+
+  let daysRemaining = totalDaysRequired - daysCurrentlyServed;
+</script>
+
+
+
+<div class="container">
+  <h2>{name} {#if daysRemaining && percentDifference > 0}<small>{daysRemaining} days remaining</small>{/if}</h2>
+
+  <progress
+    id={`progress bar for ${name}`}
+    max="100"
+    value={percentDifference}>{percentDifference}%</progress>
+
+  <div></div>
+</div>
+
+
+
+<style>
+  .container {
+    position: relative;
+    margin-bottom: 45px;
+  }
+
+  h2 small {
+    font-style: italic;
+    font-size: 60;
+  }
+
+  progress {
+    width: 100%;
+  }
+
+  progress::-moz-progress-bar { background: blue; }
+  progress::-webkit-progress-value { background: blue; }
+  progress { color: blue; }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Member from "$lib/components/Member.svelte";
+
+  const timeline = [
+    {name: "Jin", start: "12/13/2022", end: "6/13/2024", color: ""},
+    {name: "Hobi", start: "04/17/2023", end: "10/17/2024", color: ""},
+    {name: "Namjoon", start: "", end: "", color: ""},
+    {name: "Yoongi", start: "", end: "", color: ""},
+    {name: "Jimin", start: "", end: "", color: ""},
+    {name: "Taehyung", start: "", end: "", color: ""},
+    {name: "Jungkook", start: "", end: "", color: ""},
+  ]
+</script>
+
+
+<div class="container">
+  <h1>Patiently Waiting</h1>
+
+  {#each timeline as time}
+    <Member name={time.name} start={time.start} end={time.end} />
+  {/each}
+</div>
+
+
+<style>
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+</style>
